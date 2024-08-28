b897f01bc93b0bdf40a6a5a1a9cc68dcb53944a8
Juliano Pereira <juliano340@gmail.com>
2024-08-28 09:01:54 -0300

    Ajustando posicionamento de footer.

--- src/pages/desafio.tsx
+++ src/pages/desafio.tsx
@@ -3,15 +3,21 @@ import Header from '@/components/Header';
 import React from 'react';
 import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
 const Container = styled.div`
   padding: 20px;
   max-width: 800px;
   margin: 0 auto;
   text-align: center;
-  min-height: 500px;
+  flex: 1; /* Faz o container crescer e ocupar o espaço disponível */
   display: flex;
   flex-direction: column;
-  justify-content: center
+  justify-content: center;
 `;
 
 const Title = styled.h1`
@@ -29,20 +35,22 @@ const Text = styled.p`
 const Desafio: React.FC = () => {
   return (
     <>
-    <Header />
-    <Container>
-      <Title>Desafio Lacrei Saúde</Title>
-      <Text>
-        Este é um desafio para demonstrar habilidades em desenvolvimento web usando Next.js e styled-components.
-        A proposta é criar um projeto com uma interface moderna e responsiva, seguindo as diretrizes fornecidas pela Lacrei Saúde.
-      </Text>
-      <Text>
-        O objetivo é criar componentes reutilizáveis, aplicar estilos consistentes e garantir uma experiência de usuário
-        fluida e acessível em diferentes dispositivos. Este projeto serve como uma oportunidade para mostrar habilidades técnicas
-        em um cenário realista.
-      </Text>
-    </Container>
-    <Footer />
+      <Header />
+      <Wrapper>
+        <Container>
+          <Title>Desafio Lacrei Saúde</Title>
+          <Text>
+            Este é um desafio para demonstrar habilidades em desenvolvimento web usando Next.js e styled-components.
+            A proposta é criar um projeto com uma interface moderna e responsiva, seguindo as diretrizes fornecidas pela Lacrei Saúde.
+          </Text>
+          <Text>
+            O objetivo é criar componentes reutilizáveis, aplicar estilos consistentes e garantir uma experiência de usuário
+            fluida e acessível em diferentes dispositivos. Este projeto serve como uma oportunidade para mostrar habilidades técnicas
+            em um cenário realista.
+          </Text>
+        </Container>
+        <Footer />
+      </Wrapper>
     </>
   );
 };
